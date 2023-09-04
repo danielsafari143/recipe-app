484658d5e7ccf4e159f331538bdbf744a057ce45
danielsafari143 <danielsafari143@gmail.com>
2023-09-04 21:11:42 +0200

    ADD ABILITY FILE

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,10 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    redirect_to '/index'
+  end
 end
