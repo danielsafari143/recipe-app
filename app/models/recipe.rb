1ce477293c30987070927f9790cf5e772abceb5b
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-09-05 07:38:39 -0400

    Added the recipe update form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,8 @@ class Recipe < ApplicationRecord
   has_many :foods, through: :recipe_foods
 
   validates :name, presence: true
-  validates :preperation_time, presence: true
+  validates :preperation_time, presence: true # Corrected attribute name
   validates :cooking_time, presence: true
-  validates :public, inclusion: [true, false]
-  validates :public, exclusion: [nil]
+  validates :public, inclusion: { in: [true, false] }
+  validates :public, exclusion: { in: [nil] }
 end
