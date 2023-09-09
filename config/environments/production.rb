cb962ac28b10cdf9953f915ab32ead8338b9e824
danielsafari143 <danielsafari143@gmail.com>
2023-09-09 10:51:07 +0200

    add changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '100.20.92.101
-    44.225.181.72
-    44.227.217.144'}
+  config.action_mailer.default_url_options = { host: '100.20.92.101'}
 end
