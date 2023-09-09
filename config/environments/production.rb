75da6bc8ace5f91d5fab761f44ec30195bfe256b
danielsafari143 <danielsafari143@gmail.com>
2023-09-09 10:56:50 +0200

    changess

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '100.20.92.101'}
+  config.action_mailer.default_url_options = {
+    host: 'https://recipe-91ve.onrender.com',
+    port: 3000
+  }
 end
